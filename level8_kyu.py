70e42159f80c22fd86084efd5ebc49e81b3239e7
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2022-06-03 23:29:33 -0300

    Update and rename level 8kyu to level8_kyu.py

--- level8_kyu.py
+++ level8_kyu.py
@@ -32,5 +32,21 @@ def get_grade(s1, s2, s3):
     return "F"
 
 """
+Desafio 2
+
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 """
+
+def remove_every_other(my_list):
+        
+    first_elements = []
+    for value in my_list[0:len(my_list):2]:
+        first_elements.append(value)
+        
+    return first_elements
+
