5850a1642062d0c6edea1b32576cb388debedcc9
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2022-06-05 12:34:22 -0300

    Update level8_kyu.py

--- level8_kyu.py
+++ level8_kyu.py
@@ -70,3 +70,25 @@ def friend(x):
     
     friend_list = [name for name in x if len(name) == 4]
     return friend_list
+
+"""
+Desafio 4
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+"""
+def get_sum(a, b):
+
+    numbers = [a, b]
+    lst = [n for n in range(min(numbers), max(numbers) + 1, 1)]
+
+    return sum(lst)
