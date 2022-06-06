c9058bea4b2a90f9dafb36abebca02a9ceba0b30
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2022-06-06 14:01:14 -0300

    Update level6_kyu.py

--- level6_kyu.py
+++ level6_kyu.py
@@ -1,4 +1,4 @@
-"""Desafio 2
+"""Desafio 1
 
 Task
 You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
@@ -8,3 +8,35 @@ Examples
 [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 """
+
+
+
+
+
+
+
+
+"""
+Desafio 2
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+"""
+def duplicate_count(text):
+    func_counter = {}
+
+    for value in text.lower():
+        func_counter[value] = func_counter.get(value, 0) + 1
+
+    result = [result for result in func_counter.values() if result > 1]
+    return len(result)
