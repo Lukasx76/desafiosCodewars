4676119a727221788cee783f0ba41b3e351e8715
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2022-06-07 09:44:25 -0300

    Update and rename level6_kyu.py to count_duplicates.py

--- count_duplicates.py
+++ count_duplicates.py
@@ -1,24 +1,4 @@
-"""Desafio 1
-
-Task
-You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
-
-Examples
-[7, 1]  =>  [1, 7]
-[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
-[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 """
-
-
-
-
-
-
-
-
-"""
-Desafio 2
-
 Count the number of Duplicates
 Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
