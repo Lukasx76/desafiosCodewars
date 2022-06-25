4725e27b8fdd2c90d965881a93f16ca0729f6fef
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2022-06-24 23:10:38 -0300

    Create average.py

---
+++ average.py
@@ -0,0 +1,10 @@
+"""
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+"""
+
+from statistics import mean
+
+def find_average(numbers):
+    return mean(numbers)
