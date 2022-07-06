979adb126d1a6549cd8202864a2e1b30113a5b0d
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2022-07-05 23:13:24 -0300

    Create is_my_friend_cheating.py
    I have to optimize this code

---
+++ is_my_friend_cheating.py
@@ -0,0 +1,53 @@
+"""
+A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
+Within that sequence, he chooses two numbers, a and b.
+He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+Given a number n, could you tell me the numbers he excluded from the sequence?
+The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+
+[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+
+[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order of the "a".
+
+It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+
+Examples:
+removNb(26) should return [(15, 21), (21, 15)]
+or
+removNb(26) should return { {15, 21}, {21, 15} }
+or
+removeNb(26) should return [[15, 21], [21, 15]]
+or
+removNb(26) should return [ {15, 21}, {21, 15} ]
+or
+removNb(26) should return "15 21, 21 15"
+or
+
+in C:
+removNb(26) should return  {{15, 21}{21, 15}} tested by way of strings.
+Function removNb should return a pointer to an allocated array of Pair pointers, each one also allocated.
+"""
+
+
+from itertools import permutations
+from functools import reduce, lru_cache
+
+@lru_cache()
+def remov_nb(n):
+    perm = permutations(range(1, n + 1), 2)
+    result = []
+    for combination in perm:
+        var = reduce(lambda x, y: x * y, combination)
+        numbers_to_sum = list(range(1, n + 1))
+
+        for element in combination:
+            numbers_to_sum.remove(element)
+
+        if var == sum(numbers_to_sum):
+            result.append(combination)
+
+        else:
+            continue
+
+    return result
